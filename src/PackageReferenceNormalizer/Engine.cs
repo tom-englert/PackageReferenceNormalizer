4e5f58901cc4a0690c70a974f47b1dec714d01a2
tom-englert <mail@tom-englert.de>
2022-07-14 14:53:56 +0200

    Show some better error message when program fails.

---
+++ src/PackageReferenceNormalizer/Engine.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Linq;
+
+internal static class Engine
+{
+    public static void NormalizeFile(string fileName)
+    {
+        using var file = new XmlFile(fileName);
+
+        var doc = file.Document;
+
+        var packageReferenceNodes = doc.Descendants().Where(item => item.Name.LocalName == "PackageReference"
+                                                                 && item.Parent?.Name.LocalName == "ItemGroup"
+                                                                 && item.Parent?.Parent == doc.Root);
+
+        foreach (var packageReferenceNode in packageReferenceNodes)
+        {
+            packageReferenceNode.Normalize("Version");
+
+            var privateAssets = packageReferenceNode.Normalize("PrivateAssets");
+
+            if (string.Equals(privateAssets, "All", StringComparison.OrdinalIgnoreCase))
+            {
+                packageReferenceNode.Remove("IncludeAssets");
+            }
+            else
+            {
+                packageReferenceNode.Normalize("IncludeAssets");
+            }
+        }
+
+        var propertyGroupNodes = doc.Descendants().Where(item => item.Name.LocalName == "PropertyGroup"
+                                                              && item.Parent == doc.Root);
+
+        var emptyPropertyGroupNodes = propertyGroupNodes.Where(item => !item.Descendants().Any());
+
+        foreach (var emptyPropertyGroupNode in emptyPropertyGroupNodes.ToArray())
+        {
+            emptyPropertyGroupNode.Remove();
+        }
+    }
+
+    private static string? Normalize(this XElement packageReferenceNode, string itemName)
+    {
+        var element = packageReferenceNode
+            .Descendants()
+            .SingleOrDefault(item =>
+                string.Equals(item.Name.LocalName, itemName, StringComparison.OrdinalIgnoreCase) && item.Parent == packageReferenceNode);
+
+        if (element != null)
+        {
+            var descendants = element.DescendantNodes();
+            var value = descendants.OfType<XText>().FirstOrDefault()?.Value;
+            if (!string.IsNullOrEmpty(value))
+            {
+                element.Remove();
+                packageReferenceNode.Add(new XAttribute(itemName, value));
+            }
+        }
+
+        return packageReferenceNode.Attribute(itemName)?.Value;
+    }
+
+    private static void Remove(this XElement packageReferenceNode, string itemName)
+    {
+        var element = packageReferenceNode
+            .Descendants()
+            .SingleOrDefault(item =>
+                string.Equals(item.Name.LocalName, itemName, StringComparison.OrdinalIgnoreCase) && item.Parent == packageReferenceNode);
+
+        element?.Remove();
+
+        var attribute = packageReferenceNode.Attribute(itemName);
+
+        attribute?.Remove();
+    }
+}
--- src/PackageReferenceNormalizer/Program.cs
+++ src/PackageReferenceNormalizer/Program.cs
@@ -14,80 +14,20 @@ if (string.IsNullOrEmpty(directoryName))
 {
     directoryName = ".";
 }
+var searchPattern = Path.GetFileName(fileSpec);
 
-foreach (var fileName in Directory.EnumerateFiles(directoryName, Path.GetFileName(fileSpec), SearchOption.AllDirectories))
+try
 {
-    NormalizeFile(fileName);
-}
-
-static void NormalizeFile(string fileName)
-{
-    using var file = new XmlFile(fileName);
-
-    var doc = file.Document;
-
-    var packageReferenceNodes = doc.Descendants().Where(item => item.Name.LocalName == "PackageReference"
-                                                             && item.Parent?.Name.LocalName == "ItemGroup"
-                                                             && item.Parent?.Parent == doc.Root);
-
-    foreach (var packageReferenceNode in packageReferenceNodes)
+    foreach (var fileName in Directory.EnumerateFiles(directoryName, searchPattern, SearchOption.AllDirectories))
     {
-        Normalize(packageReferenceNode, "Version");
+        Console.WriteLine(fileName);
 
-        var privateAssets = Normalize(packageReferenceNode, "PrivateAssets");
-
-        if (string.Equals(privateAssets, "All", StringComparison.OrdinalIgnoreCase))
-        {
-            Remove(packageReferenceNode, "IncludeAssets");
-        }
-        else
-        {
-            Normalize(packageReferenceNode, "IncludeAssets");
-        }
-    }
-
-    var propertyGroupNodes = doc.Descendants().Where(item => item.Name.LocalName == "PropertyGroup"
-                                                          && item.Parent == doc.Root);
-
-    var emptyPropertyGroupNodes = propertyGroupNodes.Where(item => !item.Descendants().Any());
-
-    foreach (var emptyPropertyGroupNode in emptyPropertyGroupNodes.ToArray())
-    {
-        emptyPropertyGroupNode.Remove();
+        Engine.NormalizeFile(fileName);
     }
 }
-
-static string? Normalize(XElement packageReferenceNode, string itemName)
+catch (Exception ex)
 {
-    var element = packageReferenceNode
-        .Descendants()
-        .SingleOrDefault(item =>
-            string.Equals(item.Name.LocalName, itemName, StringComparison.OrdinalIgnoreCase) && item.Parent == packageReferenceNode);
-
-    if (element != null)
-    {
-        var descendants = element.DescendantNodes();
-        var value = descendants.OfType<XText>().FirstOrDefault()?.Value;
-        if (!string.IsNullOrEmpty(value))
-        {
-            element.Remove();
-            packageReferenceNode.Add(new XAttribute(itemName, value));
-        }
-    }
-
-    return packageReferenceNode.Attribute(itemName)?.Value;
+    Console.WriteLine("Error: " + ex.Message);
+    Console.WriteLine("Program terminated!");
 }
 
-static void Remove(XElement packageReferenceNode, string itemName)
-{
-    var element = packageReferenceNode
-        .Descendants()
-        .SingleOrDefault(item =>
-            string.Equals(item.Name.LocalName, itemName, StringComparison.OrdinalIgnoreCase) && item.Parent == packageReferenceNode);
-
-    element?.Remove();
-
-    var attribute = packageReferenceNode.Attribute(itemName);
-
-    attribute?.Remove();
-}
